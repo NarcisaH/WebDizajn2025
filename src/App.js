324c03af91fbf3cacc888839c5714b8bdb6beedb
Narcisa Hadžajlić <75080129+NarcisaH@users.noreply.github.com>
2025-05-06 23:33:25 +0200

    Sedmica 10
    Kreiranje osnovne strukture projekta.

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import './styles/Colors.css';
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function About() {
+  return (
+    <div>
+      <h1>Naš cilj jeste da pokažemo kako geometrija i matematika čine ljepotu islamske arhitekture!</h1>
+    </div>
+  );
+}
+
+export default About;
+
+
+
+
